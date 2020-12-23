638110069c1610365eae6dae7628f124acc984c8
Barrington <bjrochie81@gmail.com>
2020-12-22 22:18:06 -0500

    Update index.php and aboutus.php files

--- aboutus.php
+++ aboutus.php
@@ -4,36 +4,41 @@
 
 
 ?>
-<h1 class="text-center">About Us
+<h1 class="text-center">About Us</h1>
 <br>
-
-<img src="images/phpClusterTop.jpg">
-
-</h1>
-<div class="row">
-<div class="col"></div>
-<div class="col">
-
-<div class="card" style="width: 50rem;">
-
-  <div class="card-body">
-    <p class="card-text">
-        Royale Estate, Mandeville is set in Jamaica and offers a warm  
-        wealth of comfort and safety for its residents. Royale Estates is one of the first   
-        truly gated community in Mandeville.The community has two (2) clusters with its own 
-        executive body. Each of the two (2) clusters is named after Queen Elizabeth I and Cleopatra 
-        VII Philopator respectively.
-    </p>
-    <p class="card-text">
-        The Royale Estate is comprised of a board of directors, all of whom
-        are volunteers and homeowners, various committees, in addition to a property manager and
-        supporting staff that help oversee the day to day operations of the community.
-    </p>
-  </div>
-</div>
-</div>
-<div class="col"></div>
-</div>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+          <div class="carousel-inner">
+              <div class="carousel-item active">
+                  <img src="images/phpClusterTop.jpg" class="d-block w-100" alt="..." style="height:360px;">
+              </div>
+              <div class="carousel-item">
+                  <img src="images/poolarea.jpg" class="d-block w-100" alt="..." style="height:360px;">
+              </div>
+          </div>
+              <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+              </a>
+    </div>
+    <br>
+    <div class="about-section">
+        <p>
+            Royale Estate, Mandeville is set in Jamaica and offers a warm  
+            wealth of comfort and safety for its residents. Royale Estates is one of the first   
+            truly gated community in Mandeville.The community has two (2) clusters with its own 
+            executive body. Each of the two (2) clusters is named after Queen Elizabeth I and Cleopatra 
+            VII Philopator respectively.
+        </p>
+        <p>
+            The Royale Estate is comprised of a board of directors, all of whom
+            are volunteers and homeowners, various committees, in addition to a property manager and
+            supporting staff that help oversee the day to day operations of the community.
+        </p>
+    </div>
 <br>
 <?php require_once 'includes/footer.php'; ?>
 <br>
--- index.php
+++ index.php
@@ -4,24 +4,25 @@
    // require_once 'db/conn.php'; 
 ?>
 
-<div class="row">
-<div class="col"></div>
-<div class="col">
-<div class="container">
-    <h1 class="text-center">Royale Estate Application
-        <br>
-        <br>
-        <img s src="images/phpblueprint.jpg" alt="Girl in a jacket" style="width:800px;height:450px;">
-        </style>
-    </h1>
-</div></div>
-<div class="col"></div>
-</div>
-
-<?php
-    
-?>
-
+    <h1 class="text-center">Royale Estate Application</h1>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/phpblueprint.jpg" class="d-block w-100" alt="..." style="height:500px;">
+            </div>
+            <div class="carousel-item">
+                <img src="images/phpClusterTop.jpg" class="d-block w-100" alt="..." style="height:500px;">
+            </div>
+        </div>
+            <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+    </div>
 <br>
 <br>
 <br>
